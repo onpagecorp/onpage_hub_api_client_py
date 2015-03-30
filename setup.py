d57993c81697494ca6e6d4f24e9057d23f033552
Andrew Ben <andrew.p.ben@gmail.com>
2015-03-30 15:49:30 +0300

    Edit README file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'Onset Technology'
 
 setup(
     name='OnPage HUB API Client',
-    version='0.1.0',
+    version='0.1.1',
     author='Onset Technology',
     author_email='support@onsettechnology.com',
     packages=['onpage_hub_api_client'],
